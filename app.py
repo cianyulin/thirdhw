690dd0a9dd50e8f91e7d7db8402695f54fe7b40e
cian <cianyulin920@gmail.com>
2025-05-20 12:15:57 +0800

    buildfileandtryindex

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+import logging
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "<h1>Hello, World!</h1>"
+@app.route('/')
+#首頁
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+#登入
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+#註冊
+def register():
+    return render_template('register.html')
+
+@app.route('/edit_profile')
+#編輯個人資料
+def edit_profile():
+    return render_template('edit_profile.html')
+
